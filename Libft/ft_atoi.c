cbcf3d3e89db2a2cc834c24956b4ca73f97d92d8
Juliette Barbay <barbayjuliette95@gmail.com>
2023-08-28 10:48:16 +0800

    ft_atoi

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	long	num;
+	int		sign;
+	int		i;
+
+	num = 0;
+	sign = 1;
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (str[i] && ft_isdigit(str[i]))
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	return (sign * num);
+}
