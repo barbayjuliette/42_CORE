e0e02bc45fd47a68679db4f82879ca53cf063429
Juliette Barbay <jbarbay@c2r6s12.42singapore.sg>
2023-09-12 18:04:08 +0800

    print int

--- Printf/ft_printf.c
+++ Printf/ft_printf.c
@@ -11,6 +11,37 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	num;
+	char	c;
+
+	num = n;
+	if (num < 0)
+	{
+		num = -num;
+		ft_putchar_fd('-', fd);
+	}
+	if (num > 9)
+	{
+		ft_putnbr_fd(num / 10, fd);
+		ft_putnbr_fd(num % 10, fd);
+	}
+	else
+	{
+		c = '0' + num;
+		ft_putchar_fd(c, fd);
+	}
+}
 
 static int	count_args(const char *format)
 {
@@ -35,16 +66,64 @@ static int	count_args(const char *format)
 	return (count);
 }
 
+static int	put_arg(char c)
+{
+	if (c == 'd' || c == 'i' || c == 'x' || c == 'X')
+		return (1);
+	else if (c == 'c')
+		return (2);
+	else if (c == 's')
+		return (3);
+	else if (c == 'p')
+		return (4);
+	else if (c == 'u')
+		return (5);
+	else
+		return (0);
+}
+
 int ft_printf(const char *format, ...)
 {
-	int	count;
+	int		count;
+	int		i;
+	int		type;
+	va_list	ptr;
 
 	count = count_args(format);
-	return (count);
+	i = 0;
+	va_start(ptr, format);
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1] != '%')
+		{
+			type = put_arg(format[i + 1]);
+			if (type == 0)
+				return (1); // Error finish program
+			else if (type == 1) // This is an int
+			{
+				ft_putnbr_fd(va_arg(ptr, int), 1);
+			}
+			else
+				return (0);
+			i += 2;
+		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			write(1, "%", 1);
+			i += 2;
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			i++;			
+		}
+	}
+	va_end(ptr);
+	return (0);
 }
 
 int main(void)
 {	
-	printf("Number of arguments: %d", count_args("This %t%%%f%t%y%y%%%r is %d a test %%"));
+	ft_printf("This is %d test %d %% tt %d", 1, 2, 3 );
 	return 0;
 }
