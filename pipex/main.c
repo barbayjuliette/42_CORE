c9ecb5d94e071ad56d682d11f8b4d410559b56b2
Juliette Barbay <jbarbay@c2r6s12.42singapore.sg>
2023-11-08 12:09:26 +0800

    it works

--- pipex/main.c
+++ pipex/main.c
@@ -14,13 +14,16 @@
 
 int main(int argc, char *argv[], char *envp[])
 {
-	char	*infile;
-	char	*outfile;
+	char	*file1;
+	char	*file2;
 	char	**command1;
 	char	**command2;
 	int		pid1;
 	int		pid2;
 	char	*path;
+	char	*path2;
+	int		infile;
+	int		outfile;
 
 	if (argc != 5)
 	{
@@ -28,10 +31,10 @@ int main(int argc, char *argv[], char *envp[])
 		return (1);
 	}
 
-	infile = argv[1];
+	file1 = argv[1];
 	command1 = ft_split(argv[2], ' ');
 	command2 = ft_split(argv[3], ' ');
-	outfile = argv[4];
+	file2 = argv[4];
 
 	int fd[2];
 	if (pipe(fd) == -1)
@@ -46,16 +49,20 @@ int main(int argc, char *argv[], char *envp[])
 		perror("Error in main");
 		return (1);
 	}
-	char	*ls[] = {"-l", NULL};
+
 	if (pid1 == 0)
 	{
-		// Child 1: cmd1
+		// CHILD 1: cmd1
 		ft_printf("Child process 1\n");
-		path = ft_strjoin("/bin/", command1[0]);
-		// ft_putendl_fd(path, 1);
+		infile = open(file1, O_RDONLY | O_CREAT);
+		dup2(infile, 0);
+		dup2(fd[1], 1);
+		close(fd[0]);
+		close(fd[1]);
+		path = ft_strjoin("/usr/bin/", command1[0]);
 		if (execve(path, command1, envp) == -1)
 			perror("Child 1");
-		// bin or sbin ?
+		return (0);
 	}
 
 	// Main process here
@@ -70,12 +77,24 @@ int main(int argc, char *argv[], char *envp[])
 
 	if (pid2 == 0)
 	{
-		// Child 2: cmd2
+		// CHILD 2: cmd2
 		ft_printf("Child process 2\n");
+		outfile = open(file2, O_WRONLY | O_CREAT, 00777);
+		if (outfile == -1)
+			perror("Child 2, trying to open file2");
+		dup2(outfile, 1);
+		dup2(fd[0], 0);
+		close(fd[0]);
+		close(fd[1]);
+		path2 = ft_strjoin("/usr/bin/", command2[0]);
+		if (execve(path2, command2, envp) == -1)
+			perror("Child 2");
 		return (0);
 	}
 
 	// Main process
+	close(fd[0]);
+	close(fd[1]);
 	waitpid(pid2, NULL, 0);
 	ft_printf("Main process waited for children\n");
 	return (0);
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -17,8 +17,12 @@
 # include "libft/ft_printf/ft_printf.h"
 # include <unistd.h>
 # include <stdlib.h>
-#	include <stdio.h>
-# include "errno.h"
+# include <stdio.h>
+# include <errno.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+
 
 int main(int argc, char *argv[], char *envp[]);
 
