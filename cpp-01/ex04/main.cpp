93351b5b7effa7cac392ceda71b4a30ad95c6c72
Juliette Barbay <barbayjuliette95@gmail.com>
2024-03-15 11:55:08 +0800

    ex04 okay

--- cpp-01/ex04/main.cpp
+++ cpp-01/ex04/main.cpp
@@ -3,93 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jbarbay <jbarbay@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jbarbay <jbarbay@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 10:56:22 by jbarbay           #+#    #+#             */
-/*   Updated: 2024/03/14 18:14:21 by jbarbay          ###   ########.fr       */
+/*   Updated: 2024/03/15 11:52:03 by jbarbay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-
-
-// void replace_line(std::string line, std::ofstream& replace_line, char const *argv[])
-// {
-// 	std::string	s1 = argv[2];
-// 	std::string	s2 = argv[3];
-// 	int	i = 0;
-// 	int	s1_len = s1.length();
-
-// 	int line_len = line.length();
-
-// 	while (i < line_len)
-// 	{
-// 		int pos = line.find(s1, i);
-// 		if (pos == i)
-// 		{
-// 			replace_line << s2;
-// 			i += s1_len;
-// 		}
-// 		else
-// 		{
-// 			replace_line << line[i];
-// 			i++;
-// 		}
-// 	}
-// 	replace_line << "\n";
-// }
-
-void replace_text(std::ofstream& replace_file, std::ifstream& my_file, char const *argv[])
+void replace_text(std::string str, char const *argv[])
 {
 	std::string	s1 = argv[2];
 	std::string	s2 = argv[3];
 	int			i = 0;
 	int			s1_len = s1.length();
-	char		character;
 
-	while (my_file.get(character))
+	std::ofstream outfile("replace.txt");
+	if (!outfile.is_open())
+		std::cerr << "Failed to create output file" << std::endl;
+
+	while (str[i])
 	{
-		int pos = line.find(s1, i);
+		int pos = str.find(s1, i);
 		if (pos == i)
 		{
-			replace_file << s2;
+			outfile << s2;
 			i += s1_len;
 		}
 		else
 		{
-			replace_file << line[i];
+			outfile << str[i];
 			i++;
 		}
 	}
+	outfile.close();
 }
 
 int main(int argc, char const *argv[])
 {
-	std::string	filename = argv[1];
-	std::string	line;
-	std::string	new_line;
+	std::string	str;
+	char	c;
 
 	if (argc != 4)
 	{
-		std::cout << "Wrong number of arguments" << std::endl;
+		std::cerr << "Wrong number of arguments" << std::endl;
 		return (1);
 	}
-
-	std::ofstream replace_file("replace.txt");
-	std::ifstream my_file(filename);
-
-	replace_text(replace_file, my_file, argv);
-	// while (getline(my_file, line))
-	// {
-	// 	replace_line(line, replace_file, argv);
-	// }
-	my_file.close();
-	replace_file.close();
-
+	std::ifstream infile(argv[1]);
+	if (!infile.is_open())
+	{
+		std::cerr << "Failed to open file" << std::endl;
+	}
+	while (infile.get(c))
+		str += c;
+	infile.close();
+	replace_text(str, argv);
 	return 0;
 }
-
-// Error: file does not exit
-// Error: failed to open
