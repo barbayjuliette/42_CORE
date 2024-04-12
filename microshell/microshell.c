6ed62c351d6842dac119fed317db6ed6316b803a
Juliette Barbay <jbarbay@c1r7s7.42singapore.sg>
2024-04-12 16:34:42 +0800

    return status

--- microshell/microshell.c
+++ microshell/microshell.c
@@ -3,14 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   microshell.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jbarbay <jbarbay@student.42singapore.sg    +#+  +:+       +#+        */
+/*   By: jbarbay <jbarbay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 15:33:11 by jbarbay           #+#    #+#             */
-/*   Updated: 2024/04/11 23:27:00 by jbarbay          ###   ########.fr       */
+/*   Updated: 2024/04/12 16:33:41 by jbarbay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "microshell.h"
+
+int	ft_error(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(2, &str[i], 1);
+		i++;
+	}
+	return (1);
+}
 
 int	ft_strlen(char *str)
 {
@@ -21,23 +34,19 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-void	ft_cd(char **argv, int i)
+int	ft_cd(char **argv, int i)
 {
 	if (i != 2)
 	{
 		write(2, "error: cd: bad arguments\n", 25);
-		return ;
+		return (0);
 	}
 	if (chdir(argv[1]) == -1)
-	{
-		printf("ERROR\n");
-		write(2,  "error: cd: cannot change directory to ", 38);
-		write(2, argv[1], ft_strlen(argv[1] + 1));
-		write(2, "\n", 1);
-	}
+		return (ft_error("error: cd cannot change directory to "), ft_error(argv[1]), ft_error("\n"));
+	return (0);
 }
 
-void	execute(char **argv, char **envp, int i)
+int	execute(char **argv, char **envp, int i)
 {
 	int	has_pipe;
 	int	fd[2];
@@ -49,7 +58,7 @@ void	execute(char **argv, char **envp, int i)
 	if (has_pipe && pipe(fd) == -1)
 		 write(2, "error: fatal\n", 13);
 
-	// Fork to create child if there is a child
+	// Fork to create child
 	pid = fork();
 	if (pid == 0) // Child
 	{
@@ -57,11 +66,12 @@ void	execute(char **argv, char **envp, int i)
 		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 			write(2, "error: fatal\n", 13);
 		execve(*argv, argv, envp);
-		write(2, "error: cannot execute\n", 22);
+		return (ft_error("cannot execute"));
 	}
 	waitpid(pid, &status, 0);
 	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 		write(2, "error: fatal\n", 13);
+	return WIFEXITED(status) && WEXITSTATUS(status);
 }
 
 int	get_n_args(char **argv)
@@ -75,13 +85,12 @@ int	get_n_args(char **argv)
 
 int main(int argc, char *argv[], char *envp[])
 {
-	// int	status;
+	int	status;
 	int	i;
-	// int	n = 0;
 
 	i = 0;
 	if (argc == 1)
-		return (0);
+		return (1);
 
 	while (argv[i] && argv[i + 1])
 	{
@@ -91,9 +100,9 @@ int main(int argc, char *argv[], char *envp[])
 		i = get_n_args(argv);
 
 		if (strcmp(*argv, "cd") == 0)
-			ft_cd(argv, i);
+			status = ft_cd(argv, i);
 		else if (i)
-			execute(argv, envp, i);
+			status = execute(argv, envp, i);
 	}
-	return (0);
+	return (status);
 }
