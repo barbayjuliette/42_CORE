25cd96b88619efa769cbe2e9c528e4aeda226a9e
Juliette Barbay <jbarbay@c2r6s12.42singapore.sg>
2024-06-03 20:17:08 +0800

    improvements ex00

--- CPP-07/ex01/iter.hpp
+++ CPP-07/ex01/iter.hpp
@@ -1,39 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jbarbay <jbarbay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/03 19:05:18 by jbarbay           #+#    #+#             */
-/*   Updated: 2024/06/03 19:44:59 by jbarbay          ###   ########.fr       */
+/*   Created: 2024/06/03 19:26:47 by jbarbay           #+#    #+#             */
+/*   Updated: 2024/06/03 19:36:04 by jbarbay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __WHATEVER_H__
-#define __WHATEVER_H__
+#ifndef __ITER_H__
+#define __ITER_H__
 
 #include <iostream>
 
-template <typename T> 
-void swap(T& x, T& y)
+template <typename T> void iter()
 {
-	T temp = x;
 	
-	x = y;
-	y = temp;
-}
-
-template <typename T> 
-T const & max(T const & x, T const & y)
-{
-	return ((x > y) ? x : y);
-}
-
-template <typename T> 
-T const & min(T const & x, T const & y)
-{
-	return ((x < y) ? x : y);
 }
 
 #endif
