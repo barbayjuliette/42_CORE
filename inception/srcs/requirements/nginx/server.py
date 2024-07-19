06f0c78858453f4e957628a1b2d82d942d8ce64e
Juliette Barbay <jbarbay@c2r6s12.42singapore.sg>
2024-07-19 14:51:27 +0800

    can test requests not secure

---
+++ inception/srcs/requirements/nginx/server.py
@@ -0,0 +1,23 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import cgi
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        
+        # Echo back the received data
+        self.wfile.write(post_data)
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
