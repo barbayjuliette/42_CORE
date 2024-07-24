59ee968e26043346c0d9888ca43ba99c38cefc51
Juliette Barbay <jbarbay@c2r6s14.42singapore.sg>
2024-07-24 17:45:23 +0800

    still not working

--- inception_2/srcs/requirements/mariadb/tools/init.sql
+++ inception_2/srcs/requirements/mariadb/tools/init.sql
@@ -1,4 +1,11 @@
-CREATE DATABASE wordpress;
-CREATE USER 'wpuser'@'%' IDENTIFIED BY 'password';
+-- Create the database only if it does not already exist
+CREATE DATABASE IF NOT EXISTS wordpress;
+
+-- Create the user only if it does not already exist
+CREATE USER IF NOT EXISTS 'wpuser'@'%' IDENTIFIED BY 'password';
+
+-- Grant privileges to the user
 GRANT ALL PRIVILEGES ON *.* TO 'wpuser'@'%' WITH GRANT OPTION;
+
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
